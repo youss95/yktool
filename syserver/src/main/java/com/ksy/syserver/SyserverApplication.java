744f94067f395a9d91bb17a3a3a380d8f870c66e
youss95 <usekim7555@gmail.com>
2021-08-15 00:30:07 +0900

    git init

---
+++ syserver/src/main/java/com/ksy/syserver/SyserverApplication.java
@@ -0,0 +1,13 @@
+package com.ksy.syserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyserverApplication.class, args);
+	}
+
+}
---
+++ syserver/src/test/java/com/ksy/syserver/SyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksy.syserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
