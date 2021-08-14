81595847691449b3f45c10bc2c2de2fe69c619e1
youss95 <usekim7555@gmail.com>
2021-08-15 02:46:24 +0900

    validation 추가

--- syserver/src/main/java/com/ksy/syserver/domain/Project.java
+++ syserver/src/main/java/com/ksy/syserver/domain/Project.java
@@ -2,12 +2,17 @@ package com.ksy.syserver.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,13 +26,21 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message = "이름을 입력하세요")
 	private String projectName;
+	@NotBlank(message = "식별자 필요")
+	@Size(min=4, max=5,message = "4 에서 5글자")
+	@Column(updatable = false,unique = true)
 	private String projectIdentifier;
+	@NotBlank(message = "빈 칸 은 노노")
 	private String description;
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date start_date;
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date end_date;
-	
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date create_At;
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date update_At;     //date tracking
 	
 	//리스너
