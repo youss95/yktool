575afddbc4d755b22e7370bac2b9405bf505d41b
youss95 <usekim7555@gmail.com>
2021-08-15 13:52:31 +0900

    binding result, map으로 field : msg 추출

--- syserver/src/main/java/com/ksy/syserver/web/ProjectController.java
+++ syserver/src/main/java/com/ksy/syserver/web/ProjectController.java
@@ -1,8 +1,16 @@
 package com.ksy.syserver.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +25,20 @@ public class ProjectController {
 
 	@Autowired
 	private ProjectService projectService;
-	
+	//bindingresult = 에러가 있는지 없는지 
 	@PostMapping("")
-	public ResponseEntity<?> createNewProject(@RequestBody Project project){
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,BindingResult result){
+		if(result.hasErrors()) {
+			//field와 message 꺼내기
+			Map<String,String> errorMap = new HashMap<>();
+			for(FieldError error : result.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
+		}
+		
+		
+		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1,HttpStatus.CREATED);
 	}
